58536bd861e1812b47fe4c0555e1350d4652dc24
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-02-15 20:21:11 +0100

    DEMO: Adding 'FieldConfig' modal.

---
+++ demo/src/layout/FieldConfig.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app_modal">
+
+    </div>
+</template>
+<style lang="scss" scoped>
+.app_modal {
+    position: absolute;
+    left: 0;
+    top: 0;
+    min-height: 100vh;
+    min-width: 100vh;
+    width: 100%;
+    height: 100%;
+    z-index: 99;
+}
+</style>
--- demo/src/stores/counter.ts
+++ demo/src/stores/counter.ts
@@ -1,12 +1,9 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
+export const useState = defineStore('useState', () => {
+  const modal = ref<'config' | undefined>()
+  const showModal = (m: typeof modal.value) => modal.value = m
+  
+  return { modal, showModal }
 })
